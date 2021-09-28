557a10c690976c019b9adc31f38961138ef3d58e
Christian Knecht <christian.knecht@iao.fraunhofer.de>
2021-09-28 16:28:16 +0200

    feature(client): add element custom-form-item-date-time handling strings in date-time format

---
+++ client/src/resources/elements/custom-form-items/custom-form-item-date-time.js
@@ -0,0 +1,36 @@
+import {BasicComposableExtended} from 'library-aurelia/src/prototypes/basic-composable-extended';
+
+/**
+ * @extends BasicComposableExtended
+ * @category resources
+ * @subcategory custom-elements
+ *
+ * @example
+ * <require from="library-aurelia/src/resources/elements/au-form/au-form-item-date-time"></require>
+ *
+ * <compose ref="formItemCompose"
+ *          model.bind="{object: object,
+ *                       propertyKey: propertyKey,
+ *                       propertySchema: propertySchema,
+ *                       index: index}"
+ *          view-model="${composeViewModel()}"
+ *          view="${composeViewModel() + '.html'}">
+ * </compose>
+ */
+class CustomFormItemDateTime extends BasicComposableExtended {
+
+    dateTime = '';
+
+    dateTimeChanged() {
+        let date;
+        try {
+            date = new Date(this.dateTime).toISOString();
+        } catch (error) {
+            //silently catch error
+        }
+        this.object[this.propertyKey] = date;
+    }
+
+}
+
+export {CustomFormItemDateTime};
