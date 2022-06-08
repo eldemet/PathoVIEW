12484d384be060812e166a15e0e0688bd6d12cda
Christian Knecht <christian.knecht@iao.fraunhofer.de>
2022-06-08 17:39:15 +0200

    refactor(client): make map view more robust

--- client/src/views/map/map.js
+++ client/src/views/map/map.js
@@ -12,45 +12,60 @@ class MapView extends BasicViewExtended {
     async attached() {
         await this.loadAlerts();
         await this.loadDevices();
-        this.layers = {
-            overlay: [
-                {
-                    id: this.i18n.tr('model.alert', {count: 0}),
-                    type: 'layerGroup',
-                    layers: this.alerts.map(alert => {
-                        return {
-                            id: alert.id,
-                            type: 'geoJSON',
-                            data: alert.location,
-                            popupContent: `<h6><i class="${alertUtilities.getSeverityIcon(alert.severity)}"></i> ${alert.name}</h6>
-                                           ${this.i18n.tr('enum.alert.category.' + alert.category)}, ${this.i18n.tr('enum.alert.subCategory.' + alert.subCategory)}`
-                        };
-                    })
-                },
-                {
-                    id: this.i18n.tr('model.device', {count: 0}),
-                    type: 'layerGroup',
-                    layers: this.devices.map(device => {
-                        return {
-                            id: device.id,
-                            type: 'geoJSON',
-                            data: device.location,
-                            popupContent: `<h6><i class="${deviceUtilities.getDeviceIcon(device)}"></i> ${device.name}</h6>
-                                           ${this.i18n.tr('enum.device.category.' + device.category)}`
-                        };
-                    })
-                }
-            ]
-        };
+        this.setLayers();
     }
 
-    @catchError('app-alert')
+    setLayers() {
+        this.layers = {overlay: []};
+        if (this.alerts) {
+            this.layers.overlay.push({
+                id: this.i18n.tr('model.alert', {count: 0}),
+                type: 'layerGroup',
+                layers: this.alerts.map(alert => {
+                    return {
+                        id: alert.id,
+                        type: 'geoJSON',
+                        data: alert.location,
+                        popupContent: `<h6><i class="${alertUtilities.getSeverityIcon(alert.severity)}"></i> ${alert.name}</h6>
+                                           ${this.i18n.tr('enum.alert.category.' + alert.category)}, ${this.i18n.tr('enum.alert.subCategory.' + alert.subCategory)}`
+                    };
+                })
+            });
+        }
+        if (this.devices) {
+            this.layers.overlay.push({
+                id: this.i18n.tr('model.device', {count: 0}),
+                type: 'layerGroup',
+                layers: this.devices.map(device => {
+                    return {
+                        id: device.id,
+                        type: 'geoJSON',
+                        data: device.location,
+                        popupContent: `<h6><i class="${deviceUtilities.getDeviceIcon(device)}"></i> ${device.name}</h6>
+                                           ${this.i18n.tr('enum.device.category.' + device.category)}`
+                    };
+                })
+            });
+        }
+    }
+
+    @catchError('app-alert', {
+        type: 'warning',
+        message: 'alerts.general.arrayEmpty',
+        translateOptions: {type: 'alert'},
+        dismissible: true
+    })
     @loadingEvent('app-alert', 'alert')
     async loadAlerts() {
         this.alerts = (await this.proxy.get('alert').getObjects()).objects;
     }
 
-    @catchError('app-alert')
+    @catchError('app-alert', {
+        type: 'warning',
+        message: 'alerts.general.arrayEmpty',
+        translateOptions: {type: 'device'},
+        dismissible: true
+    })
     @loadingEvent('app-alert', 'device')
     async loadDevices() {
         this.devices = (await this.proxy.get('device').getObjects()).objects;
