2a8e6c85dfc9126cae9146aa3af3d74055705029
Christian Knecht <christian.knecht@iao.fraunhofer.de>
2022-10-14 15:56:52 +0200

    refactor(server): only support OpenAPI 3 in weather path handler

--- server/api/v1/paths/weather/current.js
+++ server/api/v1/paths/weather/current.js
@@ -8,13 +8,13 @@ const createError = require('http-errors');
  * @module paths/weather/current
  * @category paths
  */
-module.exports = function(config, getApiDoc) {
+module.exports = function(config) {
 
     let operations = {
         GET: logger.catchErrors(GET)
     };
 
-    operations.GET['apiDoc'] = getApiDoc(getObjectUiSchema);
+    operations.GET['apiDoc'] = getCurrentWeatherSchema;
 
     async function GET(req, res) {
         let params = Object.assign({}, req.query, {appid: config.server.openWeatherMapApiKey});
@@ -36,174 +36,170 @@ module.exports = function(config, getApiDoc) {
     return operations;
 };
 
-const getObjectUiSchema = {
-    'general': {
-        summary: 'Returns weather data from OpenWeatherMap',
-        operationId: 'getCurrentWeather'
-    },
-    '3.0.2': {
-        parameters: [
-            {
-                in: 'query',
-                name: 'lat',
-                description: '',
-                schema: {
-                    type: 'number'
-                },
-                required: true
+const getCurrentWeatherSchema = {
+    summary: 'Returns weather data from OpenWeatherMap',
+    operationId: 'getCurrentWeather',
+    parameters: [
+        {
+            in: 'query',
+            name: 'lat',
+            description: '',
+            schema: {
+                type: 'number'
             },
-            {
-                in: 'query',
-                name: 'lon',
-                description: '',
-                schema: {
-                    type: 'number'
-                },
-                required: true
+            required: true
+        },
+        {
+            in: 'query',
+            name: 'lon',
+            description: '',
+            schema: {
+                type: 'number'
             },
-            {
-                in: 'query',
-                name: 'units',
-                description: '',
-                schema: {
-                    type: 'string',
-                    enum: [
-                        'standard', // Kelvin °K
-                        'imperial', // Fahrenheit °F
-                        'metric' // Celsius °C
-                    ]
-                },
-                required: false
+            required: true
+        },
+        {
+            in: 'query',
+            name: 'units',
+            description: '',
+            schema: {
+                type: 'string',
+                enum: [
+                    'standard', // Kelvin °K
+                    'imperial', // Fahrenheit °F
+                    'metric' // Celsius °C
+                ]
             },
-            {
-                in: 'query',
-                name: 'mode',
-                description: '',
-                schema: {
-                    type: 'string',
-                    enum: [
-                        'xml',
-                        'html'
-                        // do not set parameter for JSON
-                    ]
-                },
-                required: false
+            required: false
+        },
+        {
+            in: 'query',
+            name: 'mode',
+            description: '',
+            schema: {
+                type: 'string',
+                enum: [
+                    'xml',
+                    'html'
+                    // do not set parameter for JSON
+                ]
             },
-            {
-                in: 'query',
-                name: 'lang',
-                description: '',
-                schema: {
-                    type: 'string'
-                },
-                required: false
+            required: false
+        },
+        {
+            in: 'query',
+            name: 'lang',
+            description: '',
+            schema: {
+                type: 'string'
             },
-            {
-                $ref: '#/components/parameters/language'
-            }
-        ],
-        responses: {
-            200: {
-                description: 'Success',
-                content: {
-                    'application/json': {
-                        schema: {
-                            type: 'object',
-                            required: [
-                                'coord',
-                                'weather',
-                                'base',
-                                'main',
-                                'visibility',
-                                'wind',
-                                'clouds',
-                                'dt',
-                                'sys',
-                                'timezone',
-                                'id',
-                                'name',
-                                'cod'
-                            ],
-                            properties: {
-                                coord: {
-                                    type: 'object',
-                                    required: ['lat', 'lon'],
-                                    properties: {
-                                        lat: {type: 'number'},
-                                        lon: {type: 'number'}
-                                    }
-                                },
-                                weather: {
-                                    type: 'array',
-                                    items: {
-                                        required: [
-                                            'id',
-                                            'main',
-                                            'description',
-                                            'icon'
-                                        ],
-                                        properties: {
-                                            id: {type: 'number'},
-                                            main: {type: 'string'},
-                                            description: {type: 'string'},
-                                            icon: {type: 'string'}
-                                        }
-                                    }
-                                },
-                                base: {type: 'string'},
-                                main: {
-                                    type: 'object',
-                                    properties: {
-                                        temp: {type: 'number'},
-                                        feels_like: {type: 'number'},
-                                        temp_min: {type: 'number'},
-                                        temp_max: {type: 'number'},
-                                        pressure: {type: 'number'},
-                                        humidity: {type: 'number'}
-                                    }
-                                },
-                                visibility: {type: 'number'},
-                                wind: {
-                                    type: 'object',
+            required: false
+        },
+        {
+            $ref: '#/components/parameters/language'
+        }
+    ],
+    responses: {
+        200: {
+            description: 'Success',
+            content: {
+                'application/json': {
+                    schema: {
+                        type: 'object',
+                        required: [
+                            'coord',
+                            'weather',
+                            'base',
+                            'main',
+                            'visibility',
+                            'wind',
+                            'clouds',
+                            'dt',
+                            'sys',
+                            'timezone',
+                            'id',
+                            'name',
+                            'cod'
+                        ],
+                        properties: {
+                            coord: {
+                                type: 'object',
+                                required: ['lat', 'lon'],
+                                properties: {
+                                    lat: {type: 'number'},
+                                    lon: {type: 'number'}
+                                }
+                            },
+                            weather: {
+                                type: 'array',
+                                items: {
                                     required: [
-                                        'speed',
-                                        'deg'
+                                        'id',
+                                        'main',
+                                        'description',
+                                        'icon'
                                     ],
                                     properties: {
-                                        speed: {type: 'number'},
-                                        deg: {type: 'number'}
-                                    }
-                                },
-                                clouds: {
-                                    type: 'object',
-                                    properties: {
-                                        all: {type: 'number'}
-                                    }
-                                },
-                                dt: {type: 'number'},
-                                sys: {
-                                    type: 'object',
-                                    properties: {
-                                        type: {type: 'number'},
                                         id: {type: 'number'},
-                                        message: {type: 'number'},
-                                        country: {type: 'string'},
-                                        sunrise: {type: 'number'},
-                                        sunset: {type: 'number'}
+                                        main: {type: 'string'},
+                                        description: {type: 'string'},
+                                        icon: {type: 'string'}
                                     }
-                                },
-                                timezone: {type: 'number'},
-                                id: {type: 'number'},
-                                name: {type: 'string'},
-                                cod: {type: 'number'}
-                            }
+                                }
+                            },
+                            base: {type: 'string'},
+                            main: {
+                                type: 'object',
+                                properties: {
+                                    temp: {type: 'number'},
+                                    feels_like: {type: 'number'},
+                                    temp_min: {type: 'number'},
+                                    temp_max: {type: 'number'},
+                                    pressure: {type: 'number'},
+                                    humidity: {type: 'number'}
+                                }
+                            },
+                            visibility: {type: 'number'},
+                            wind: {
+                                type: 'object',
+                                required: [
+                                    'speed',
+                                    'deg'
+                                ],
+                                properties: {
+                                    speed: {type: 'number'},
+                                    deg: {type: 'number'}
+                                }
+                            },
+                            clouds: {
+                                type: 'object',
+                                properties: {
+                                    all: {type: 'number'}
+                                }
+                            },
+                            dt: {type: 'number'},
+                            sys: {
+                                type: 'object',
+                                properties: {
+                                    type: {type: 'number'},
+                                    id: {type: 'number'},
+                                    message: {type: 'number'},
+                                    country: {type: 'string'},
+                                    sunrise: {type: 'number'},
+                                    sunset: {type: 'number'}
+                                }
+                            },
+                            timezone: {type: 'number'},
+                            id: {type: 'number'},
+                            name: {type: 'string'},
+                            cod: {type: 'number'}
                         }
                     }
                 }
-            },
-            default: {
-                $ref: '#/components/responses/Error'
             }
+        },
+        default: {
+            $ref: '#/components/responses/Error'
         }
     }
 };
