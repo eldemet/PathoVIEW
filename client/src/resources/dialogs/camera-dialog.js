a4fe0bfdd9b94db382b9090f98c191002c644503
Christian Knecht <christian.knecht@iao.fraunhofer.de>
2022-11-11 00:28:30 +0100

    feature(client): add camera dialog

---
+++ client/src/resources/dialogs/camera-dialog.js
@@ -0,0 +1,29 @@
+import {BasicDialog} from 'library-aurelia/src/prototypes/basic-dialog';
+
+/**
+ * @extends BasicDialog
+ * @category resources
+ * @subcategory dialogs
+ */
+class CameraDialog extends BasicDialog {
+
+    imageData;
+
+    /**
+     * @param {ConstructorParameters<typeof BasicDialog>} rest
+     */
+    constructor(...rest) {
+        super(...rest);
+    }
+
+    close(isCancel) {
+        if (isCancel) {
+            this.dialogController.cancel();
+        } else {
+            this.dialogController.ok({imageData: this.imageData});
+        }
+    }
+
+}
+
+export {CameraDialog};
