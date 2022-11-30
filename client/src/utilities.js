6a6f4b73fdaf8c31d0848aff34afdff3f861ad53
Christian Knecht <christian.knecht@iao.fraunhofer.de>
2022-11-30 15:58:40 +0100

    bugfix(client): catch error if abbreviation cannot be parsed

--- client/src/utilities.js
+++ client/src/utilities.js
@@ -212,11 +212,15 @@ export const userUtilities = {
     getAbbreviation(user) {
         let abbreviation = 'ND';
         if (user) {
-            let name = user.username;
-            if (user.firstName && user.lastName) {
-                name = user.firstName + ' ' + user.lastName;
+            try {
+                let name = user.username;
+                if (user.firstName && user.lastName) {
+                    name = user.firstName + ' ' + user.lastName;
+                }
+                abbreviation = name.match(/(^\S\S?|\s\S)?/g).map(v => v.trim()).join('').match(/(^\S|\S$)?/g).join('').toLocaleUpperCase();
+            } catch (error) {
+                //silently handle error
             }
-            abbreviation = name.match(/(^\S\S?|\s\S)?/g).map(v => v.trim()).join('').match(/(^\S|\S$)?/g).join('').toLocaleUpperCase();
         }
         return abbreviation;
     }
