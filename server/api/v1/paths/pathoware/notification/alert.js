e106190b50e67f010cb0443116e94fb7823e70cb
Christian Knecht <christian.knecht@iao.fraunhofer.de>
2023-01-26 13:27:22 +0100

    feature(server): add endpoint to post notifications from PathoWARE if an alert was created

---
+++ server/api/v1/paths/pathoware/notification/alert.js
@@ -0,0 +1,53 @@
+import Logger from 'utilities-node/src/utilities/logger.js';
+
+export default function(notificationService, getApiDoc) {
+
+    const logger = new Logger(import.meta);
+
+    let operations = {
+        POST: logger.catchErrors(POST)
+    };
+
+    operations.POST.apiDoc = getApiDoc(publishNotification);
+
+    async function POST(req, res) {
+        let notification = {content: req.body.data, senderId: req.body.subscriptionId, topic: 'model', contentType: 'alert', operationType: 'create'};
+        res.send(notificationService.publishNotification(notification));
+    }
+
+    return operations;
+}
+
+
+const publishNotification = {
+    summary: 'publish a notification',
+    operationId: 'publishNotification',
+    parameters: [],
+    requestBody: {
+        required: true,
+        content: {
+            'application/json': {
+                schema: {
+                    type: 'object',
+                    properties: {
+                        subscriptionId: {
+                            type: 'string'
+                        },
+                        data: {
+                            type: 'object'
+                        }
+                    }
+                }
+            }
+        }
+    },
+    responses: {
+        200: {
+            $ref: '#/components/responses/NotificationPublished'
+        },
+        default: {
+            $ref: '#/components/responses/Error'
+        }
+    }
+};
+
