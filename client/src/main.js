44609f23f9f5a16b810a059806c06fac57e73b89
Christian Knecht <christian.knecht@iao.fraunhofer.de>
2023-10-05 12:16:04 +0200

    refactor(client): restructure main for better readability, add comments

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,12 @@
 import 'bootstrap';
+import {I18N} from 'aurelia-i18n';
 import {PLATFORM} from 'aurelia-pal';
 import {AureliaFramework} from 'library-aurelia/src/framework';
+import {AureliaCookie} from 'aurelia-cookie';
 import {EventAggregator} from 'aurelia-event-aggregator';
 import {HttpService} from 'library-aurelia/src/services/http-service';
-import {I18N} from 'aurelia-i18n';
+import {ModelServiceBasic} from 'library-aurelia/src/services/model-service-basic';
+import {ModelServiceContextAware} from './services/model-service-context-aware';
 // @ts-ignore
 import environment from '../config/environment.json';
 // @ts-ignore
@@ -14,14 +17,13 @@ import {BhapticsServiceImplementation} from './services/bhaptics-service-APP_TAR
 import {NotificationServiceImplementation} from './services/notification-service-APP_TARGET';
 // @ts-ignore
 import {ContextServiceImplementation} from './services/context-service-APP_TARGET';
-import {ModelServiceBasic} from 'library-aurelia/src/services/model-service-basic';
-import {ModelServiceContextAware} from './services/model-service-context-aware';
-import {AureliaCookie} from 'aurelia-cookie';
 
+// set model service options
 const apiEntrypoint = '/api/v1/model';
 const uniqueProperty = 'id';
 /** @type {import('library-aurelia/types/types').AjvFormats[]} **/
 const formats = ['uri-reference', 'date-time'];
+const deviceFormats = [...formats, ...['uri', 'ipv4', 'ipv6']];
 const getSchema = ':api-entrypoint/:type/schema-ui';
 const getSchemaDevice = getSchema.replace(':api-entrypoint', apiEntrypoint).replace(':type', 'device');
 const getObjects = ':api-entrypoint/:type:filter-query';
@@ -34,10 +36,20 @@ if (environment.usePathoware) {
     alertOptions.endpoints = {getSchema, getObjects};
 }
 
+/**
+ * @param {import('aurelia-framework').Aurelia }aurelia
+ * @return {Promise<void>}
+ */
 export async function configure(aurelia) {
-    let authService = aurelia.container.get(AuthServiceImplementation);
-    await authService.start(environment.keycloak, environment.testing);
-    let contextService = aurelia.container.get(ContextServiceImplementation);
+    // get instances of required dependencies
+    const authService = aurelia.container.get(AuthServiceImplementation);
+    const contextService = aurelia.container.get(ContextServiceImplementation);
+    const notificationService = aurelia.container.get(NotificationServiceImplementation);
+    const httpService = aurelia.container.get(HttpService);
+    const eventAggregator = aurelia.container.get(EventAggregator);
+    const i18n = aurelia.container.get(I18N);
+
+    // set framework initialization options
     let root = PLATFORM.moduleName('app');
     let globalResources = [
         PLATFORM.moduleName('resources/elements/custom-form-items/custom-form-item-date-time'),
@@ -52,17 +64,14 @@ export async function configure(aurelia) {
         PLATFORM.moduleName('resources/elements/custom-detail-properties/custom-detail-property-role'),
         PLATFORM.moduleName('resources/elements/custom-detail-properties/custom-detail-property-status')
     ];
-    const httpService = aurelia.container.get(HttpService);
-    const eventAggregator = aurelia.container.get(EventAggregator);
-    const i18n = aurelia.container.get(I18N);
     let registerServices = [
         authService,
         contextService,
-        aurelia.container.get(NotificationServiceImplementation),
+        notificationService,
         new ModelServiceContextAware('alert', alertOptions, httpService, i18n, eventAggregator),
         new ModelServiceContextAware('mission', {apiEntrypoint, uniqueProperty, formats, filterProperty: 'refId', endpoints: {getObjects}}, httpService, i18n, eventAggregator),
         new ModelServiceContextAware('annotation', {apiEntrypoint, uniqueProperty, formats, filterProperty: 'source', endpoints: {getObjects}}, httpService, i18n, eventAggregator),
-        new ModelServiceBasic('device', {apiEntrypoint, uniqueProperty, formats: [...formats, ...['uri', 'ipv4', 'ipv6']], endpoints: {getSchema: getSchemaDevice}, throwOnValidationError: false}, httpService, i18n, eventAggregator),
+        new ModelServiceBasic('device', {apiEntrypoint, uniqueProperty, formats: deviceFormats, endpoints: {getSchema: getSchemaDevice}, throwOnValidationError: false}, httpService, i18n, eventAggregator),
         new ModelServiceBasic('emergency-event', {apiEntrypoint, uniqueProperty, formats}, httpService, i18n, eventAggregator)
     ];
     if (environment.enableBhaptics) {
@@ -71,6 +80,8 @@ export async function configure(aurelia) {
     let registerPlugins = [
         PLATFORM.moduleName('aurelia-animator-css')
     ];
+    let splashScreen = (await import('views/splash-screen.html?raw')).default;
+
     //TODO remove if library-aurelia is updated
     if (!AureliaCookie.get('search-alert-sort-by')) {
         AureliaCookie.set('search-alert-sort-by', 'dateIssued', {});
@@ -80,6 +91,10 @@ export async function configure(aurelia) {
         AureliaCookie.set('search-mission-sort-by', 'createdAt', {});
         AureliaCookie.set('search-mission-sort-ascending', 'false', {});
     }
-    let splashScreen = (await import('views/splash-screen.html?raw')).default;
+
+    // redirect to keycloak for login
+    await authService.start(environment.keycloak, environment.testing);
+
+    // initialize framework
     await AureliaFramework.initialize(aurelia, {environment, root, globalResources, registerServices, registerPlugins, splashScreen});
 }
