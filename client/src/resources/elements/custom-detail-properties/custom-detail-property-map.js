05575474fe51d28bdc4f13da1f9549f0707995bb
Christian Knecht <christian.knecht@iao.fraunhofer.de>
2023-10-06 17:56:33 +0200

    feature(client): add popup with coordinates and map link to custom-detail-property-map

--- client/src/resources/elements/custom-detail-properties/custom-detail-property-map.js
+++ client/src/resources/elements/custom-detail-properties/custom-detail-property-map.js
@@ -1,10 +1,11 @@
 import {v1 as uuid} from 'uuid';
 import {BasicComposable} from 'library-aurelia/src/prototypes/basic-composable';
-import {locationUtilities} from '../../../utilities';
+import {locationUtilities, alertUtilities} from '../../../utilities';
 
 class CustomDetailPropertyMap extends BasicComposable {
 
     value;
+    object;
     propertyKey;
     schema;
     layer;
@@ -19,6 +20,20 @@ class CustomDetailPropertyMap extends BasicComposable {
 
     activate(model) {
         super.activate(model);
+        let popupContent;
+        let utilities;
+        try {
+            if (this.object.type === 'Alert') {
+                utilities = alertUtilities;
+            }
+        } catch (error) {
+            // silently handle error
+        }
+        try {
+            popupContent = locationUtilities.getMapLinkContent(this.value);
+        } catch (error) {
+            // object does not proper location with coordinates
+        }
         if (this.value) {
             try {
                 this.center = locationUtilities.getCenter(this.value);
@@ -33,7 +48,9 @@ class CustomDetailPropertyMap extends BasicComposable {
                         data: this.value,
                         settings: {
                             fitBounds: true
-                        }
+                        },
+                        options: utilities?.getOptions?.(),
+                        popupContent: popupContent
                     }
                 ]
             };
