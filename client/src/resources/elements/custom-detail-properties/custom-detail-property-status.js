42aa8f8df4fa142444c007978db4258ca8671a9e
Christian Knecht <christian.knecht@iao.fraunhofer.de>
2023-10-06 18:18:25 +0200

    feature(client): use timeout in toggle mission status function of custom-detail-property-status to avoid unnecessary calls

--- client/src/resources/elements/custom-detail-properties/custom-detail-property-status.js
+++ client/src/resources/elements/custom-detail-properties/custom-detail-property-status.js
@@ -6,8 +6,15 @@ class CustomDetailPropertyStatus extends BasicComposable {
     schema;
     object;
 
+    timeout;
+
     async toggleStatus(status) {
+        if (this.timeout) clearTimeout(this.timeout);
         this.value = this.schema.enum[(this.schema.enum.indexOf(status) + 1) % this.schema.enum.length];
+        this.timeout = setTimeout(() => this.saveStatus(), 2000);
+    }
+
+    async saveStatus() {
         await this.proxy.get('mission').updateObject(Object.assign({}, this.object, {[this.propertyKey]: this.value}));
         this.object[this.propertyKey] = this.value;
     }
